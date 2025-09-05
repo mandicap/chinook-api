16d848b40463591ddde840d7c337054d4c0d641e
Amanda Romero <mandicap@comcast.net>
2025-09-04 18:14:38 -0600

    refactor: make string or number into reusable schema

--- utils/schema/index.ts
+++ utils/schema/index.ts
@@ -1,22 +1,13 @@
 import z from 'zod';
 
+export const stringOrNumberSchema = z.union([
+    z.number(), // number or
+    z
+        .string()
+        .transform((str) => parseInt(str, 10)), // string transformed to number
+]);
+
 export const querySchema = z.object({
-    limit: z
-        .union([
-            z.number(), // number or
-            z
-                .string()
-                .transform((str) => parseInt(str, 10)), // string transformed to number
-        ])
-        .optional()
-        .default(10),
-    page: z
-        .union([
-            z.number(), // number or
-            z
-                .string()
-                .transform((str) => parseInt(str, 10)), // string transformed to number
-        ])
-        .optional()
-        .default(1),
+    limit: stringOrNumberSchema.optional().default(10),
+    page: stringOrNumberSchema.optional().default(1),
 });
